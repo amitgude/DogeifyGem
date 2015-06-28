271f53d3580bd455241f195c74822d6710df25b6
Amit <gudeamit148@gmail.com>
2015-06-28 07:25:54 +0530

    created main process method and also created core extensions for string and array

---
+++ lib/dogeify.rb
@@ -0,0 +1,30 @@
+require "dogeify/version"
+require "engtagger"
+require "dogeify/all"
+class Dogeify
+	ADJECTIVES = %w[so such very many much].freeze
+  def process(str)
+  	str = str.downcase
+
+    tagger = EngTagger.new
+    tagged_str = tagger.add_tags(str)
+    phrases = tagger.get_nouns(tagged_str).keys
+
+    phrases = phrases.each_with_index.map do |noun,i|
+     
+     "#{adjective(i)} #{noun}"
+
+    end
+
+    phrases << "wow"
+
+    phrases.join(' ')
+
+  end
+
+  private
+
+  def adjective(i)
+  	ADJECTIVES[i % ADJECTIVES.size]
+  end
+end
